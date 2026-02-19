7b5030f585bada94b8c9b6cc0d6cf2d75b5a8ef5
its-yash0003 <yashgautam2407@gmail.com>
2026-02-20 00:29:50 +0530

    video 55 completed !

---
+++ 55-Varisbles, data type and objects/script.js
@@ -0,0 +1,35 @@
+console.log("hey this is tutorial 55");
+
+var a= 5;
+let b = 6;
+let c = "yash";
+let _a = "yashu";
+// var 55a = 5;   // not allowed
+
+{
+    let a = 55;
+    console.log(a);
+}
+console.log(a);
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+"name": "Harry",
+"job code": 5600,
+"is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
