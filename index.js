1f63ed499c937de7dc486b3283a8345220868660
its-yash0003 <yashgautam2407@gmail.com>
2026-02-28 23:50:23 +0530

    video 56 completed !

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello i m a conditional tutorial");
+
+let age = 16;
+let grace = 2;
+
+console.log("your age is " + (age + grace));
+console.log("your age is " + (age - grace));
+console.log("your age is " + (age * grace));
+console.log("your age is " + (age / grace));
+console.log("your age is " + (age ** grace));
+console.log("your age is " + (age % grace));
+
+if ((age + grace) >= 18) {
+    console.log("you can vote");
+} 
+
+else {
+    console.log("you cannot vote");
+}
