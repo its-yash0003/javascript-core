ea5257cc14a02c917b82d52ebd3bd301043a1d38
its-yash0003 <yashgautam2407@gmail.com>
2026-03-01 23:41:24 +0530

    video 57 completed !

---
+++ 57/index.js
@@ -0,0 +1,21 @@
+console.log('this is a tutorial on loops in javascript');
+
+let a = 1;
+
+for (let i = 0; i < 100; i++) {
+    console.log(a + i);
+}
+
+let b = 1;
+while (b < 100) {
+    console.log(b);
+    b++;
+}
+    
+let c = 10;
+do {
+    console.log(c);
+    c++;
+} while (c < 100);
+
+
